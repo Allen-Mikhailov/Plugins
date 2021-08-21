5dac456c559204fca9ecc29048f7106e6640c05a
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2021-08-21 18:57:41 -0500

    Create ColorData.lua

---
+++ Slinky's Tools v2/ColorData.lua
@@ -0,0 +1,8 @@
+local module = {}
+
+module.Background = Color3.new(0.180392, 0.180392, 0.180392)
+module.Header = Color3.new(0.101961, 0.101961, 0.101961)
+module.Dark = Color3.new(0.129412, 0.129412, 0.129412)
+module.TextColor = Color3.new(0.752941, 0.752941, 0.752941)
+
+return module
